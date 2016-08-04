de801d4f0fc5803036c097c7afdee5b475debd21
Bo Zhao and Rony Nasr <bo+rony@>
2016-08-04 14:30:34 -0700

    Working wikipedia API with get meaning upon selecting a word

--- js/wikipedia-interface.js
+++ js/wikipedia-interface.js
@@ -5,8 +5,59 @@ $(document).ready(function(){
     var currentWikipedia = new Wikipedia();
     var word = $("#word").val();
     $("#word").val("");
-    var definition = currentWikipedia.search(word);
-    $("#definition").append("<h3>"+ word +"</h3>");
-    $("#definition").append("<p>" + definition +"</p>");
+    $("#word").select();
+
+    currentWikipedia.search(word, displayResults);
+
+
   });
 });
+
+$(document).on("mouseup", function (e) {
+
+  var selected = getSelection();
+  var range = selected.getRangeAt(0);
+  if(selected.toString().length > 1){
+    word = selected.toString();
+    $("#word").val(word);
+    $("#search").click();
+  }
+  selected.removeAllRanges();
+});
+
+function getSelection() {
+  var seltxt = '';
+  if (window.getSelection) {
+    seltxt = window.getSelection();
+  } else if (document.getSelection) {
+    seltxt = document.getSelection();
+  } else if (document.selection) {
+    seltxt = document.selection.createRange().text;
+  }
+  else return;
+  return seltxt;
+}
+
+function displayResults(result) {
+  $(".modal-title").text(word);
+  $(".modal-body p").text(result);
+  var htmlText = "";
+  htmlText = htmlText +
+            '<div id="result-modal" class="modal fade"   tabindex="-1"role="dialog">' +
+              '<div class="modal-dialog">' +
+                '<div class="modal-content">' +
+                  '<div class="modal-header">' +
+                    '<button type="button" class="close" data-dismiss="modal" aria-label="Close">'+
+                    '<span aria-hidden="true">&times;</span></button>' +
+                    '<h4 class="modal-title">'+ word +'</h4>' +
+                  '</div>' +
+                  '<div class="modal-body">' +
+                    '<p>' + result + '</p>'+
+                  '</div>'+
+                '</div>' +
+              '</div>' +
+            '</div>';
+  $("#definition").append(htmlText);
+  $("#result-modal").modal('show');
+  // $("#definition").show();
+}
--- js/wikipedia.js
+++ js/wikipedia.js
@@ -1,18 +1,27 @@
 function Wikipedia() {
-
 }
 
-Wikipedia.prototype.search = function (word) {
-  $wgCrossSiteAJAXdomains = Array( '*' );
-  $wgCrossSiteAJAXdomains = Array( 'en.wikipedia.org', 'en.wikibooks.org' );
-  $wgCrossSiteAJAXdomains = Array( '*.wikipedia.org' );
+Wikipedia.prototype.search = function (word,displayFunction) {
+   word = word.replace(/\b\w/g, function(l){ return l.toUpperCase() });
+  console.log(word);
+ $.ajax('https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles='+ word).then(function(response) {
 
-  $.ajax('https://en.wiktionary.org/w/api.php?action=query&titles=' + word + '&prop=revisions&rvprop=content&format=jsonfm').then(function(response) {
-    console.log(response.text);
-   $("#definition").append(response);
+   var pageid = getPageId(response);
+   var result = response.query.pages[pageid[0]].extract;
+    displayFunction(result);
+   return result;
+   console.log(result);
  }).fail(function(error) {
    return("Error!");
  });
 };
+
+function getPageId(data) {
+  var pageid = [];
+  for( var id in data.query.pages ) {
+    pageid.push( id );
+  }
+  return pageid;
+}
 
 exports.wikipediaModule = Wikipedia;
